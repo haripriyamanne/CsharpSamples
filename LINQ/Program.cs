8c7a03fecb1b5eb3f112fddb533e02a4936c7804
Haripriya <haripriyamanne4@gmail.com>
2021-05-01 22:45:49 +0530

    LINQ Updated

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -10,6 +10,19 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+           int[] marks = new int[] { 97, 92, 81, 60 ,36, 78, 89};
+
+            // Define the query expression.
+            IEnumerable<int> marksQuery =
+                from mark in marks
+                where mark > 80
+                select mark;
+
+            // Execute the query.
+            foreach (int m in marksQuery)
+            {
+                Console.Write(m + " ");
+            }
         }
     }
 }
