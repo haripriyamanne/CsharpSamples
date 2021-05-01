16c44a51898ccddfe95260370f9a458a0d9f1736
Haripriya <haripriyamanne4@gmail.com>
2021-05-01 22:36:07 +0530

    Dynamics Updated

--- Dynnamics/Program.cs
+++ Dynnamics/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace Dynnamics
 {
+    public class Student
+    {
+        public void DisplayStudentInfo(int id)
+        {
+            Console.WriteLine("Id is Executed");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            dynamic stud = new Student(); //If you assign a class object to the dynamic type, then the compiler would not
+                                          //check for correct methods and properties name of a dynamic type that holds the custom class object
+
+            stud.DisplayStudentInfo(1);
+            stud.DisplayStudentInfo(1,"Hari");// run - time error, no compile-time error
+            //stud.DisplayStudentInfo("1");// run-time error, no compile-time error
+            // stud.FakeMethod();// run-time error, no compile-time error
         }
     }
+
+    
 }
+
