fbcc7f1a39fc174be3f18faabdd1eafa6e6ff5b3
Haripriya <haripriyamanne4@gmail.com>
2021-05-01 23:01:43 +0530

    For Loop Updated with Numbers in Triangle

--- For Loop/Program.cs
+++ For Loop/Program.cs
@@ -15,18 +15,18 @@ namespace For_Loop
             int n = int.Parse(Console.ReadLine());
             for (i = 1; i <= n; i++)
             {
-                for (j = 1; j <= n - i; j++)
-                {
-                    Console.Write(" ");
-                }
-                for (k = 1; k <= i; k++)
-                {
-                    Console.Write(k);
-                }
-                for (l = i - 1; l >= 1; l--)
-                {
-                    Console.Write(l);
-                }
+                    for (j = 1; j <= n - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                        for (k = 1; k <= i; k++)
+                        {
+                            Console.Write(k);
+                        }
+                            for (l = i - 1; l >= 1; l--)
+                            {
+                                Console.Write(l);
+                            }
                 Console.Write("\n");
             }
         }
